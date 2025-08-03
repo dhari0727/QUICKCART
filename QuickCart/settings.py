f849e646913de03fc26e108f4ad22836c59e52c7
Thakkar Dharini <dharinithakkar4@gmail.com>
2025-08-03 20:58:42 +0530

    Update settings.py

--- QuickCart/settings.py
+++ QuickCart/settings.py
@@ -111,8 +111,8 @@ EMAIL_BACKEND = EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True     # TLS = True, NOT SSL
-EMAIL_HOST_USER = 'dharinithakkar4@gmail.com'
-EMAIL_HOST_PASSWORD = 'jhna uopm vmze dkmw'  # Use app password for Gmail
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ""  # Use app password for Gmail
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Internationalization
